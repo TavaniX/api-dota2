3691f5719555ebf4c32ba5aab64d5d2d1a6f9b0e
Maksim Aisin <maksim.aisin@mail.ru>
2022-06-18 12:30:43 +0300

    feat: render heroes grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+const url = 'https://api.opendota.com/api/heroStats'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [heroes, setHeroes] = useState([])
+
+    const getHeroes = async () => {
+        const response = await fetch(url)
+        const heroes = await response.json()
+        setHeroes(heroes)
+    }
+
+    useEffect(() => {
+        getHeroes()
+    }, [])
+
+    const [filteredHeroes, setFilteredHeroes] = useState(heroes)
+
+    const getAttackType = (type) => {
+        let heroesByType = heroes.filter((hero) => hero.attack_type === type)
+        !type ? setFilteredHeroes(heroes) : setFilteredHeroes(heroesByType)
+    }
+
+    return (
+        <>
+            <h3 className='title'>dota2 heroes</h3>
+            <button className='btn' onClick={() => getAttackType()}>
+                All
+            </button>
+            <button className='btn' onClick={() => getAttackType('Melee')}>
+                Melee
+            </button>
+            <button className='btn' onClick={() => getAttackType('Ranged')}>
+                Ranged
+            </button>
+            <p className='totalNumber'>Total: {filteredHeroes.length}</p>
+            <ul className='heroes'>
+                {filteredHeroes.map((hero) => {
+                    const { id, localized_name, img, attack_type } = hero
+                    const url = 'https://api.opendota.com' + img
+                    return (
+                        <li key={id}>
+                            <img src={url} alt={localized_name} />
+                            <div>
+                                <h4>{localized_name}</h4>
+                                <span>{attack_type}</span>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    )
 }
 
-export default App;
+export default App
